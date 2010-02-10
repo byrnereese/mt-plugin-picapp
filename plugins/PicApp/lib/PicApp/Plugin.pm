337159431e6a4d4d3e87e1abfb22def39723c90b
Byrne <byrne@majordojo.com>
2010-02-10 12:29:56 -0800

    minor fix to regex that inserts <script> tag

--- plugins/PicApp/lib/PicApp/Plugin.pm
+++ plugins/PicApp/lib/PicApp/Plugin.pm
@@ -25,8 +25,8 @@ sub pre_save {
     my $app = MT::App->instance;
     my $ref = ref $app;
     if ($ref eq "MT::App::CMS" && $app->mode eq 'save_entry') {
-        $obj->{column_values}->{text} =~ s{<!--PICAPPSCRIPTTAG-->}{<script type="text/javascript" src="http://cdn.pis.picapp.com/IamProd/PicAppPIS/JavaScript/PisV4.js"></script>}g;
-        $obj->{column_values}->{text_more} =~ s{<!--PICAPPSCRIPTTAG-->}{<script type="text/javascript" src="http://cdn.pis.picapp.com/IamProd/PicAppPIS/JavaScript/PisV4.js"></script>}g;
+        $obj->{column_values}->{text} =~ s{<!--PICAPPSCRIPTTAG-->}{<script type="text/javascript" src="http://cdn.pis.picapp.com/IamProd/PicAppPIS/JavaScript/PisV4.js"></script>}gm;
+        $obj->{column_values}->{text_more} =~ s{<!--PICAPPSCRIPTTAG-->}{<script type="text/javascript" src="http://cdn.pis.picapp.com/IamProd/PicAppPIS/JavaScript/PisV4.js"></script>}gm;
     }
 }
 
